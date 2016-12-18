721f610097316ac048f146b62357fbd62c93bd7a
Osman Kandemir <juanter53@hotmail.com>
2016-12-18 03:30:07 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -2,6 +2,7 @@
 #! -*- coding : utf - 8 -*-
 
 
+
 import re
 import mechanize
 import urllib
@@ -12,6 +13,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 #https://goo.gl/oFB4GJ
 #entertoken = raw_input("Please Enter The Your Token : ")
@@ -29,38 +31,49 @@ browser.find_element_by_xpath(log_but2).click()
 log_but3= "//input[@class='greyishBtn']"
 browser.find_element_by_xpath(log_but3).click()
 
+def kaydet(ca):
+	ab = browser.page_source.encode("utf-8")
+	ls = str(ab)
+	reg = re.search('<img src=.+alt=',ls)
+	kaydet1 = open("cap.txt","a+")
+	yazdir = reg.group().replace('<img src="/l/captcha.php?_CAPTCHA&amp;t=',"")[:4] + ":" + ca + "\n"
+	kaydet1.writelines(yazdir)
+	kaydet1.close()
+
+
 def fonk():
 	while(1):
 
 		head = browser.find_element_by_xpath("//input[contains(@class, 'validate[required]')]")
-		head.send_keys("https://www.facebook.com/NewBeTurkey/photos/a.551180941735681.1073741828.546524742201301/611311919055916/?type=3")
+		head.send_keys("https://www.facebook.com/NewBeTurkey/posts/611801562340285")
 		log_but4= "//input[@class='greyishBtn submitForm']"
 		browser.find_element_by_xpath(log_but4).click()
 		ca = raw_input("gir bakam : ")
 		captcha = browser.find_element_by_name("captcha")
 		captcha.send_keys(ca)
+		kaydet(ca)
 		log_but4 = "//input[@type='submit']"
 		browser.find_element_by_xpath(log_but4).click()
 		time.sleep(1)
 		while(1):
 			if 'src="index/success.png"' in browser.page_source.encode("utf-8"):
 				browser.find_element_by_xpath("//a[@href ='../']").click()
-				time.sleep(6)
+				time.sleep(4)
 				fonk()
 			elif not 'src="index/success.png"' in browser.page_source.encode("utf-8"):
 				while(1):
 					time.sleep(2)
 					if 'src="index/success.png"' in browser.page_source.encode("utf-8"):
 						browser.find_element_by_xpath("//a[@href ='../']").click()
-						time.sleep(6)
+						time.sleep(4)
 						fonk()
 					if  'Her 50 saniyede' in browser.page_source.encode("utf-8"):
 						browser.find_element_by_xpath("//a[@href ='../']").click()
-						time.sleep(6)
+						time.sleep(4)
 						fonk()
 					if "Hata Kodu:104" in browser.page_source.encode("utf-8"):
 						browser.find_element_by_xpath("//a[@href ='../']").click()
-						time.sleep(6)
+						time.sleep(4)
 						fonk()
 
 
